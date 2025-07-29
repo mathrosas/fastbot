22513683319e40458ec354d2a4e34294c8dfd2f8
mathrosas <mathiasrosas@gmail.com>
2025-07-29 14:08:27 -0300

    Import Lslidar_ROS2_driver, fastbot_description & fastbot_gazebo as regular packages

---
+++ fastbot_description/onshape/api-keys.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Replace access and secret key with your own, copied from the popup once API key was created in onshame:
+# The API key's access key is The API key's access key is ... and the secret key is ... Please transfer this securely to your application now as you will not be able to display this secret key string again. 
+export ONSHAPE_API=https://cad.onshape.com
+export ONSHAPE_ACCESS_KEY=PrxlGWexGjvlGXsZgdLId8q4
+export ONSHAPE_SECRET_KEY=xOAmK9PCZPLEA06oF1LwLAaDgq5IYcAzorRqCWTXW6xaXqWK
